ee70772b30cd813f1fbb517e93e11e6e5113b6ff
LakshayBhandari <LakshayBhandari867@gmail.com>
2023-09-15 01:27:34 +0530

    added search page & activity page

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,25 +1,27 @@
-"use client"
-import { sidebarLinks } from "@/constants";
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 import { usePathname, useRouter } from "next/navigation";
-function LeftSidebar() {
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 
+import { sidebarLinks } from "@/constants";
+
+const LeftSidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
-  const { userId } = useAuth();
 
+  const { userId } = useAuth();
 
   return (
     <section className='custom-scrollbar leftsidebar'>
-         <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
         {sidebarLinks.map((link) => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
 
-            if (link.route === "/profile") link.route = `${link.route}/${userId}`;
+          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
           return (
             <Link
@@ -57,7 +59,7 @@ function LeftSidebar() {
         </SignedIn>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
