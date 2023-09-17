1d27e52275e87e52fe9f6ffb72ee6ab88774876d
LakshayBhandari <LakshayBhandari867@gmail.com>
2023-09-18 00:01:32 +0530

    build fix

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,20 +1,25 @@
-import AccountProfile from "@/components/forms/AccountProfile";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser } from "@/lib/actions/user.action";
+import AccountProfile from "@/components/forms/AccountProfile";
 
 async function Page() {
-  
-    const user=await currentUser();
-    const userInfo={};
+  const user = await currentUser();
+  if (!user) return null; // to avoid typescript warnings
 
-    const userData = {
-        id: user.id,
-        objectId: userInfo?._id,
-        username: userInfo ? userInfo?.username : user.username,
-        name: userInfo ? userInfo?.name : user.firstName ?? "",
-        bio: userInfo ? userInfo?.bio : "",
-        image: userInfo ? userInfo?.image : user.imageUrl,
-      };
-      
+  const userInfo = await fetchUser(user.id);
+  if (userInfo?.onboarded) redirect("/");
+
+  const userData = {
+    id: user.id,
+    objectId: userInfo?._id,
+    username: userInfo ? userInfo?.username : user.username,
+    name: userInfo ? userInfo?.name : user.firstName ?? "",
+    bio: userInfo ? userInfo?.bio : "",
+    image: userInfo ? userInfo?.image : user.imageUrl,
+  };
+
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
